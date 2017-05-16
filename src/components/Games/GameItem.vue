b9c1e513a2110b11eb048841472edede594070d2
Gramatiik <stanyslas.bres@gmail.com>
2017-05-16 11:08:35 +0200

    Change : games presentation

--- src/components/Games/GameItem.vue
+++ src/components/Games/GameItem.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <router-link class="game-item-card" :to="{ name: 'GameDetails', params: {slug: gameData.slug} }">
-      <img class="game-thumb" :src="gameData.cover | cloudinary('thumb_2x')" />
+      <div class="game-thumb" :style="{ backgroundImage: cardThumb }"></div>
       <div class="game-infos">
         <h3>{{ gameData.name }}</h3>
         <span class="release-date">Released {{ new Date(gameData.first_release_date) | moment("from", "now") }}</span>
@@ -14,7 +14,12 @@
   export default {
     name: 'game-item',
     props: [ 'gameData' ],
-    methods: {}
+    methods: {},
+    computed: {
+      cardThumb: function () {
+        return `url("${this.$options.filters.cloudinary(this.gameData.cover, 'thumb_2x')}")`
+      }
+    }
   }
 </script>
 
@@ -22,7 +27,7 @@
   @import "variables";
 
   .game-item-card {
-    display: flex;
+    display: block;
     padding: 5px;
     margin-top: 10px;
     margin-bottom: 10px;
@@ -31,13 +36,11 @@
     box-shadow: 0 5px 10px $bg-dark;
 
     .game-thumb {
+      float: left;
       width: 90px;
       height: 90px;
-      object-fit: contain;
-    }
-
-    .game-infos {
-      margin-left: 5px;
+      background-size: cover;
+      margin-right: 5px;
     }
   }
 </style>
--- src/components/Games/Games.vue
+++ src/components/Games/Games.vue
@@ -2,14 +2,20 @@
   <div>
     <h1 class="page-title">{{ title }}</h1>
 
-    <div class="games-container"
-         v-infinite-scroll="loadMore"
-         infinite-scroll-disabled="canLoad"
-         infinite-scroll-distance="3">
-      <game-item class="card-item" v-for="item in games" :key="item.id" :game-data="item"></game-item>
+    <div v-if="!apiFailure">
+      <div class="games-container"
+           v-infinite-scroll="loadMore"
+           infinite-scroll-disabled="listLoading"
+           infinite-scroll-distance="3">
+        <game-item class="card-item" v-for="item in games" :key="item.id" :game-data="item"></game-item>
+      </div>
+    </div>
+    <div v-else style="text-align: center">
+      Unable to load games...
+      <button @click="retry">Try again ?</button>
     </div>
 
-    <div class="spinner" v-if="listLoading">
+    <div class="spinner" v-if="listLoading && !apiFailure">
       <div class="double-bounce double-bounce-1"></div>
       <div class="double-bounce double-bounce-2"></div>
     </div>
@@ -19,7 +25,8 @@
 
 <script>
   import { mapState, mapGetters } from 'vuex'
-  import GameItem from '@/components/GameItem'
+  import { API_FAILURE, CLEAR_GAMES } from '@/store/mutation-types'
+  import GameItem from '@/components/Games/GameItem'
   export default {
     name: 'games',
     props: [ 'ordering' ],
@@ -31,22 +38,27 @@
     computed: {
       ...mapState({
         games: state => state.gamesModule.games,
-        listLoading: state => state.listLoading
+        listLoading: state => state.listLoading,
+        apiFailure: state => state.apiFailure
       }),
-      ...mapGetters(['gamesCount', 'canLoad'])
+      ...mapGetters(['gamesCount'])
     },
     methods: {
       loadGames () {
         this.title = this.ordering === 'recent' ? 'Recent Games' : 'Popular Games'
 
         // clear saved games at component startup
-        this.$store.commit('CLEAR_GAMES')
+        this.$store.commit(CLEAR_GAMES)
 
         // load initial data
         this.$store.dispatch('getGamesList', { orderingField: this.ordering, offset: this.gamesCount })
       },
       loadMore () {
         // load more data
+        this.$store.dispatch('getGamesList', { orderingField: this.ordering, offset: this.gamesCount })
+      },
+      retry () {
+        this.$store.commit(API_FAILURE, false)
         this.$store.dispatch('getGamesList', { orderingField: this.ordering, offset: this.gamesCount })
       }
     },
