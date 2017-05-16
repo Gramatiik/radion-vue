a970855465627f72234da090b18438f214394c4f
Gramatiik <stanyslas.bres@gmail.com>
2017-05-16 10:59:07 +0200

    Add : filter in pulses api call

--- src/api/igdbPulses.js
+++ src/api/igdbPulses.js
@@ -2,13 +2,15 @@ import Vue from 'vue'
 import IgdbQueryBuilder from './igdbBase'
 
 export default {
-  getLatestPulses (orderingField, offset) {
+  getLatestPulses (offset) {
     let builder = new IgdbQueryBuilder()
 
     return Vue.http.get(builder
       .setEndpoint('pulses')
-      .setFields(['*'])
+      .setFields(['id', 'title', 'image', 'url', 'published_at'])
       .setLimit(25)
+      .setOrder({field: 'published_at', type: 'desc'})
+      .setFilters([{field: 'published_at', type: 'lte', value: Date.now()}])
       .setOffset(offset || 0)
       .finalize())
       .then(response => response.body || [])
