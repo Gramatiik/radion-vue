6a6676955777120ea62a7755ec0ede81e7174a61
Gramatiik <stanyslas.bres@gmail.com>
2017-05-19 16:00:32 +0200

    Change : game details component

--- src/components/Games/GameDetails.vue
+++ src/components/Games/GameDetails.vue
@@ -1,6 +1,25 @@
 <template>
-  <transition mode="out-in" name="slide-fade" duration="5000">
+  <transition mode="out-in" name="slide-fade">
     <div>
+      <div class="game-header">
+
+        <lazy-background
+          class="game-cover"
+          :image-source="gameCoverImage"
+          :loading-image="require('@/assets/images/pulse-no-image.png')"
+          :error-image="require('@/assets/images/pulse-no-image.png')">
+        </lazy-background>
+
+        <h1 class="game-name">{{ gameDetails.name }}</h1>
+
+        <lazy-background
+          class="game-header-background"
+          :image-source="gameCoverImage"
+          :loading-image="require('@/assets/images/pulse-no-image.png')"
+          :error-image="require('@/assets/images/pulse-no-image.png')">
+        </lazy-background>
+
+      </div>
       <pin-platforms class="game-platforms" :platform-ids="platformIds"></pin-platforms>
       <pre>{{ gameDetails }}</pre>
     </div>
@@ -25,6 +44,9 @@
           platformIds.push(release.platform)
         }
         return platformIds
+      },
+      gameCoverImage: function () {
+        return this.gameDetails.cover ? this.gameDetails.cover.url : require('@/assets/images/pulse-no-image.png')
       }
     },
     components: {
@@ -35,6 +57,9 @@
     },
     beforeRouteUpdate (to, from, next) {
       getGameDetails(to, next)
+    },
+    created () {
+      scrollTo(0, 0)
     }
   }
 
@@ -46,4 +71,48 @@
 
 <style scoped lang="scss">
   @import "variables";
+
+  .game-header {
+    position: relative;
+    display: flex;
+    flex-flow: column;
+    justify-content: center;
+
+    min-height: 350px;
+    background-size: cover;
+    overflow: hidden;
+    padding: 10px 5px;
+
+    .game-cover, .game-header-background {
+      background: $font-light center;
+    }
+
+    .game-cover {
+      box-shadow: 2px 2px 12px rgba(#000, .9);
+      width: 90px;
+      height: 128px;
+      margin: 0 auto;
+    }
+
+
+    .game-header-background {
+      position: absolute;
+      top: 0;
+      bottom: 0;
+      left: 0;
+      right: 0;
+      filter: blur(15px);
+      transform: scale(1.2);
+      z-index: -1;
+    }
+
+    .game-name {
+      text-align: center;
+      line-height: 3.3rem;
+      font-size: 3.5rem;
+      color: $font-light;
+      text-shadow: 2px 2px 5px #000;
+      margin: 25px 20px 40px;
+    }
+  }
 </style>
