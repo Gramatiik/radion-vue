7e88eb1caf2280c8e3a5006a91c8dda2a221edbf
Gramatiik <stanyslas.bres@gmail.com>
2017-06-08 19:44:35 +0200

    Add : Discover page skeleton

---
+++ src/components/Discover/DiscoverPage.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="DiscoverPage">
+
+    </div>
+</template>
+
+<script>
+  export default {
+    name: 'discover-page',
+    data () {
+      return {}
+    }
+  }
+</script>
+
+<style scoped lang="scss">
+    .DiscoverPage {
+
+    }
+</style>
