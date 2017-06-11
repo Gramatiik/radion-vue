e030349b156365f915b81d830b671e6760b98d6c
Gramatiik <stanyslas.bres@gmail.com>
2017-06-11 16:38:54 +0200

    Add : platforms details presented in modals

---
+++ src/components/Platforms/PlatformDetailsComponent.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="PlatformDetailsComponent">
+
+    </div>
+</template>
+
+<script>
+  export default {
+    name: 'platform-details-component',
+    data () {
+      return {}
+    }
+  }
+</script>
+
+<style scoped lang="scss">
+    .PlatformDetailsComponent {
+
+    }
+</style>
