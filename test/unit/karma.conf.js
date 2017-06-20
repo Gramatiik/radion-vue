451c497ec1f717532f3e9d12e4c677976a2d78b5
Gramatiik <stanyslas.bres@gmail.com>
2017-06-20 17:36:35 +0200

    Fix : Query Builder empty options bug

--- test/unit/karma.conf.js
+++ test/unit/karma.conf.js
@@ -11,7 +11,7 @@ module.exports = function (config) {
     // 1. install corresponding karma launcher
     //    http://karma-runner.github.io/0.13/config/browsers.html
     // 2. add it to the `browsers` array below.
-    browsers: [ (process.env.TRAVIS ? 'Chrome_travis' : 'Chrome') ],
+    browsers: [ process.env.TRAVIS ? 'Chrome_travis' : 'Chrome' ],
     frameworks: ['mocha', 'sinon-chai', 'phantomjs-shim'],
     reporters: ['spec', 'coverage'],
     files: ['./index.js'],
